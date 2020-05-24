f8083b92e0444eb8ccbc228a436da390673e5274
yammmt <cu.cs.y20@gmail.com>
2020-05-24 16:44:42 +0900

    Use another directory for image test
    This app uses the `photo/sample` directory for its test. Don't use
    the `photo` directory because it could contain actual data to be
    used.
    
    For the same reason, it's better to remove any images in the `photo`
    directory.

--- src/tests.rs
+++ src/tests.rs
@@ -15,22 +15,22 @@ fn test_new_window() {
 // TODO: make sure that huge image is resized to fit given size
 fn test_img_filepath() {
     // ensure all image paths are included
-    let img_filepaths = get_scaled_img_filepath_array("./photo/", WindowSize(3840, 2160)).unwrap();
-    for entry in glob("photo/*.jpg").unwrap() {
-        let filename = &entry.unwrap().to_str().unwrap().replace("photo/", "").replace("resized/", "");
+    let img_filepaths = get_scaled_img_filepath_array("./photo/test/", WindowSize(3840, 2160)).unwrap();
+    for entry in glob("photo/test/*.jpg").unwrap() {
+        let filename = &entry.unwrap().to_str().unwrap().replace("photo/test/", "").replace("resized/", "");
         assert!(img_filepaths.iter().any(|v| v.ends_with(filename)));
     }
 }
 
 #[test]
 fn test_img_filepath_with_wrong_dir() {
-    assert!(get_scaled_img_filepath_array("./photo/*.jpg", WindowSize(3840, 2160)).is_err());
+    assert!(get_scaled_img_filepath_array("./photo/test/*.jpg", WindowSize(3840, 2160)).is_err());
 }
 
 #[test]
 // TODO: avoid using directory same as develop/release build
 fn test_img_buffer() {
-    let img_buf = image_buffer_from_filepath("photo/sawayaka256.jpg");
+    let img_buf = image_buffer_from_filepath("photo/test/sawayaka256.jpg");
     // there are NO practical ways to test image buffer itself...
     assert_eq!(img_buf.width, 256);
     assert_eq!(img_buf.height, 256);
